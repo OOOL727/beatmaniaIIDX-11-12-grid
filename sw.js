93038d23373b2bd3b27fbb3e0728fc3e6eb23fe9
OOOL727 <315925913+OOOL727@users.noreply.github.com>
2026-08-16 00:11:08 +0900

    update iroiro

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lampgrid-v13";
+const CACHE = "lampgrid-v14";
 const ASSETS = [
   "./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-512-maskable.png", "./icon-180.png"
