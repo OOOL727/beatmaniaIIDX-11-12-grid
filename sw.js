85ecc3ce6cb27e85a0df010354959c03aa8e7095
OOOL727 <315925913+OOOL727@users.noreply.github.com>
2026-08-15 21:59:11 +0900

    ...

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lampgrid-v4";
+const CACHE = "lampgrid-v5";
 const ASSETS = [
   "./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-512-maskable.png", "./icon-180.png"
