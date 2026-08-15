c8c78eeda5b441f265f436599f7f768e7a2c79aa
OOOL727 <315925913+OOOL727@users.noreply.github.com>
2026-08-15 21:49:19 +0900

    add expandable tier detail

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lampgrid-v2";
+const CACHE = "lampgrid-v3";
 const ASSETS = [
   "./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-512-maskable.png", "./icon-180.png"
